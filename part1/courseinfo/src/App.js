dd0b49aa019a4cf9d65497611387550eeeb15cde
Leo Kiiski <goalaleo@gmail.com>
2021-04-11 00:57:43 +0300

    Complete exercises 1.1 and 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Header = (props) => {
+  return(
+      <h1>{props.course}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      {parts.map(part => (
+        <Part key={part[0]} part_name={part[0]} num_exercises={part[1]}/> 
+      ))}
+    </div>
+  ) 
+}
+
+const Total = ({total}) => (
+    <p>Number of exercises {total}</p>
+)
+
+const Part = ({part_name, num_exercises}) => (
+  <p>
+    {part_name} {num_exercises}
+  </p>
+)
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[
+          [part1, exercises1],
+          [part2, exercises2],
+          [part3, exercises3],
+        ]}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 }
 
-export default App;
+export default App
